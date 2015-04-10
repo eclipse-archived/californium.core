3dbdaf9b88ab103d92ac3d4b5d6f1af0457b2f09
nicom24 <nico.montali@studenti.unipr.it>
2015-04-10 18:52:16 +0200

    Update HttpTranslator.java
    Added translation compatibility for DTLS connections with coaps://

--- californium-proxy/src/main/java/org/eclipse/californium/proxy/HttpTranslator.java
+++ californium-proxy/src/main/java/org/eclipse/californium/proxy/HttpTranslator.java
@@ -452,7 +452,7 @@ public final class HttpTranslator {
 
 			if (proxyingEnabled) {
 				// if the uri hasn't the indication of the scheme, add it
-				if (!uriString.matches("^coap://.*")) {
+				if (!uriString.matches("^coap://.*") && !uriString.matches("^coaps://.*")) {
 					uriString = "coap://" + uriString;
 				}
 
